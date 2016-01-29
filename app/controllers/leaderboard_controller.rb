90804a864fc3a0f6c7d51c28b271b70d1003c868
Sirko Sittig <sirko.sittig@gmail.com>
2016-01-29 18:50:56 -0300

    add basic navigation to change weeks via link

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -1,11 +1,25 @@
 class LeaderboardController < ApplicationController
   def users
-    @leaderboards = UserLeaderboard.includes(:user).current_week
+    @leaderboards = UserLeaderboard.includes(:user).week(start_date, end_date)
     render 'partials/leaderboard_table'
   end
 
   def projects
-    @leaderboards = ProjectLeaderboard.includes(:project).current_week
+    @leaderboards = ProjectLeaderboard.includes(:project).week(start_date, end_date)
     render 'partials/leaderboard_table'
   end
+
+  private
+
+    def start_date
+      @start_date = Time.now.beginning_of_week.to_date - weeks_ago.weeks
+    end
+
+    def end_date
+      @end_date = Time.now.end_of_week.to_date - weeks_ago.weeks
+    end
+
+    def weeks_ago
+      @weeks_ago = (params[:weeks_ago] || 0).to_i
+    end
 end
---
+++ spec/controllers/leaderboard_controller_spec.rb
@@ -0,0 +1,163 @@
+require 'rails_helper'
+
+describe LeaderboardController, type: :controller do
+  include ActiveSupport::Testing::TimeHelpers
+  render_views
+
+  describe '#users' do
+    it 'renders successful' do
+      get :users
+
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe '#projects' do
+    it 'renders successful' do
+      get :projects
+
+      expect(response.status).to eq(200)
+    end
+
+    context 'leaderboard exists' do
+      let(:project) { FactoryGirl.create :project }
+      let!(:leaderboard_1) {
+        FactoryGirl.create :project_leaderboard,
+                           total_minutes: 1133,
+                           start_date: Time.new(2015, 07, 6, 0, 0, 0).to_date,
+                           end_date: Time.new(2015, 07, 12, 0, 0, 0).to_date,
+                           project: project
+      }
+      let!(:leaderboard_2) {
+        FactoryGirl.create :project_leaderboard,
+                           total_minutes: 1234,
+                           start_date: Time.new(2015, 07, 13, 0, 0, 0).to_date,
+                           end_date: Time.new(2015, 07, 19, 0, 0, 0).to_date,
+                           project: project
+      }
+
+      it 'shows current leaderboard without params' do
+        travel_to Time.new(2015, 07, 18, 0, 0, 0) do
+          get :projects
+
+          expect(response.status).to   eq(200)
+          expect(response.body).to     include("1234")
+          expect(response.body).to_not include("1133")
+          expect(response.body).to     include("2015-07-13")
+          expect(response.body).to     include("2015-07-19")
+        end
+      end
+
+      it 'shows current leaderboard for current week' do
+        travel_to Time.new(2015, 07, 18, 0, 0, 0) do
+          get :projects, weeks_ago: 0
+
+          expect(response.status).to   eq(200)
+          expect(response.body).to     include("1234")
+          expect(response.body).to_not include("1133")
+          expect(response.body).to     include("2015-07-13")
+          expect(response.body).to     include("2015-07-19")
+        end
+      end
+
+      it 'shows leaderboard from one week ago' do
+        travel_to Time.new(2015, 07, 18, 0, 0, 0) do
+          get :projects, weeks_ago: 1
+
+          expect(response.status).to  eq(200)
+          expect(response.body).to     include("1133")
+          expect(response.body).to_not include("1234")
+          expect(response.body).to     include("2015-07-06")
+          expect(response.body).to     include("2015-07-12")
+        end
+      end
+
+      it 'shows leaderboard from two weeks ago' do
+        travel_to Time.new(2015, 07, 18, 0, 0, 0) do
+          get :projects, weeks_ago: 2
+
+          expect(response.status).to eq(200)
+          expect(response.body).to_not include("1133")
+          expect(response.body).to_not include("1234")
+          expect(response.body).to     include("2015-06-29")
+          expect(response.body).to     include("2015-07-05")
+        end
+      end
+    end
+  end
+
+  describe '#users' do
+    it 'renders successful' do
+      get :users
+
+      expect(response.status).to eq(200)
+    end
+
+    context 'leaderboard exists' do
+      let(:user) { FactoryGirl.create :user }
+      let!(:leaderboard_1) {
+        FactoryGirl.create :user_leaderboard,
+                           total_minutes: 1133,
+                           start_date: Time.new(2015, 07, 6, 0, 0, 0).to_date,
+                           end_date: Time.new(2015, 07, 12, 0, 0, 0).to_date,
+                           user: user
+      }
+      let!(:leaderboard_2) {
+        FactoryGirl.create :user_leaderboard,
+                           total_minutes: 1234,
+                           start_date: Time.new(2015, 07, 13, 0, 0, 0).to_date,
+                           end_date: Time.new(2015, 07, 19, 0, 0, 0).to_date,
+                           user: user
+      }
+
+      it 'shows current leaderboard without params' do
+        travel_to Time.new(2015, 07, 18, 0, 0, 0) do
+          get :users
+
+          expect(response.status).to   eq(200)
+          expect(response.body).to     include("1234")
+          expect(response.body).to_not include("1133")
+          expect(response.body).to     include("2015-07-13")
+          expect(response.body).to     include("2015-07-19")
+        end
+      end
+
+      it 'shows current leaderboard for current week' do
+        travel_to Time.new(2015, 07, 18, 0, 0, 0) do
+          get :users, weeks_ago: 0
+
+          expect(response.status).to   eq(200)
+          expect(response.body).to     include("1234")
+          expect(response.body).to_not include("1133")
+          expect(response.body).to     include("2015-07-13")
+          expect(response.body).to     include("2015-07-19")
+        end
+      end
+
+      it 'shows leaderboard from one week ago' do
+        travel_to Time.new(2015, 07, 18, 0, 0, 0) do
+          get :users, weeks_ago: 1
+
+          expect(response.status).to  eq(200)
+          expect(response.body).to     include("1133")
+          expect(response.body).to_not include("1234")
+          expect(response.body).to     include("2015-07-06")
+          expect(response.body).to     include("2015-07-12")
+        end
+      end
+
+      it 'shows leaderboard from two weeks ago' do
+        travel_to Time.new(2015, 07, 18, 0, 0, 0) do
+          get :users, weeks_ago: 2
+
+          expect(response.status).to eq(200)
+          expect(response.body).to_not include("1133")
+          expect(response.body).to_not include("1234")
+          expect(response.body).to     include("2015-06-29")
+          expect(response.body).to     include("2015-07-05")
+        end
+      end
+    end
+  end
+
+end
