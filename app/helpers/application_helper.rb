a48d284a727d931ff5e1087611bdbcb77ec7be9d
Mauro Otonelli <mauro.otonelli@gmail.com>
2016-04-20 12:23:45 -0300

    Changed active class helper names to be more accurate.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,10 @@
 module ApplicationHelper
 
-  def users_active?
-    current_page?(action: 'users') ||
-      current_page?(root_url) ? raw(' class="active"') : ''
+  def users_active_class
+    current_page?(action: 'users') || current_page?(root_url) ? "active" : ""
   end
 
-  def projects_active?
-    current_page?(action: 'projects') ? raw(' class="active"') : ''
+  def projects_active_class
+    current_page?(action: 'projects') ? "active" : ""
   end
 end
