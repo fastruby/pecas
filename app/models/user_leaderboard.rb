67075941a06c3af0ccdfa6d8a6e2cdda5208911f
Sirko Sittig <sirko.sittig@gmail.com>
2016-04-20 13:43:55 -0300

    add with_logged_minutes scope to user leaderboard

--- app/models/user_leaderboard.rb
+++ app/models/user_leaderboard.rb
@@ -1,8 +1,12 @@
 class UserLeaderboard < ActiveRecord::Base
   include CurrentWeek
   include LeaderboardCalculation
-  
+
   belongs_to :user
+
+  def self.with_logged_minutes
+    where.not(total_minutes: 0)
+  end
 
   def name
     user.name
