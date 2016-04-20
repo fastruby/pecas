69ea3c007348174d141a593581147dc54cb21cdf
Mauro Otonelli <mauro.otonelli@gmail.com>
2016-04-20 11:44:37 -0300

    Moved code for active/inactive tab to helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def users_active?
+    current_page?(action: 'users') ||
+      current_page?(root_url) ? raw(' class="active"') : ''
+  end
+
+  def projects_active?
+    current_page?(action: 'projects') ? raw(' class="active"') : ''
+  end
 end
