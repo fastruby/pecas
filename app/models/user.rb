c296fab61466dfe87b6c9afa5437d7e7ae94e41b
Mauro Otonelli <mauro.otonelli@gmail.com>
2016-04-21 13:54:04 -0300

    Added 'active' scope to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,12 @@ class User < ActiveRecord::Base
 
   has_many :entries
 
+  scope :active, -> { where(state: "active") }
+
   def self.send_reminders
     return if Date.today.saturday? || Date.today.sunday? || holiday?
 
-    User.all.each do |user|
+    User.active.each do |user|
       if user.entries.today.count == 0
         Reminder.send_to(user).deliver
       end
@@ -14,7 +16,7 @@ class User < ActiveRecord::Base
   end
 
   private
-  
+
   def self.holiday?
     ENV["COUNTRY_CODE"] && Holidays.on(Date.today, ENV["COUNTRY_CODE"]).any?
   end
