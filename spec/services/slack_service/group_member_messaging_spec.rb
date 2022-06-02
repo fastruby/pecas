597a446afa139a700a75262b620293720a4bfc33
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2022-06-01 23:38:44 -0400

    Remove comments from spec file

--- spec/services/slack_service/group_member_messaging_spec.rb
+++ spec/services/slack_service/group_member_messaging_spec.rb
@@ -115,39 +115,4 @@ describe SlackService::GroupMemberMessaging do
       expect(list_block[:text]).to include("20 minutes")
     end
   end
-
-  # let(:user) {
-  #   SlackService::SlackUser.new({
-  #     id: "A00AA1AAA",
-  #     name: "cap",
-  #     real_name: "Steve Rogers",
-  #     first_name: "Steve",
-  #     tz: "America/New_York",
-  #     email: "steve@ombulabs.com",
-  #     client: TimeEntrySpec::SlackClientMock
-  #   })
-  # }
-
-  # let(:user_1) { create(:user, email: "steve@ombulabs.com") }
-  # let(:entry_1)   { create(:entry, user: user_1, date: Date.new(2022, 3, 1), description: "A non-descriptive description 1", minutes: 120) }
-  # let(:entry_2)   { create(:entry, user: user_1, date: Date.new(2022, 3, 1), description: "A non-descriptive description 2", minutes: 90) }
-  # let(:entry_3)   { create(:entry, user: user_1, date: Date.new(2022, 3, 1), description: "A non-descriptive description 3", minutes: 20) }
-
-  # describe ".bad_entry_message_template" do
-  #   it "formats a multi-part message to sent the user" do
-  #     message = TimeEntry.bad_entry_message_template(user, [entry_1, entry_2, entry_3])
-  #     # {:text => {:text => "The Description", :type => "plain_text"}, :type => "section"}
-  #     intro_block = message[:blocks][0][:text]
-  #     list_block = message[:blocks][1][:text]
-  #     expect(intro_block[:type]).to eql("plain_text")
-  #     expect(intro_block[:text]).to include(user.first_name)
-  #     expect(list_block[:type]).to eql("mrkdwn")
-  #     expect(list_block[:text]).to include(entry_1.description)
-  #     expect(list_block[:text]).to include("2 hours")
-  #     expect(list_block[:text]).to include(entry_2.description)
-  #     expect(list_block[:text]).to include("1 hour, 30 minutes")
-  #     expect(list_block[:text]).to include(entry_3.description)
-  #     expect(list_block[:text]).to include("20 minutes")
-  #   end
-  # end
 end
