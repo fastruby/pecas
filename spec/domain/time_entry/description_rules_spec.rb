eee270781a33d9112e8534ea1a3735cefe0e0bec
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2023-01-04 07:56:20 -0500

    fix missing specs for keywords

--- spec/domain/time_entry/description_rules_spec.rb
+++ spec/domain/time_entry/description_rules_spec.rb
@@ -45,6 +45,20 @@ describe TimeEntry::DescriptionRules do
         expect(validator.valid?).to eql(true)
       end
 
+      it "will pass if it has a keyword" do
+        entry = Entry.new(description: "This is an English class entry")
+        validator = TimeEntry::DescriptionRules.new(entry)
+        expect(validator.valid?).to eql(true)
+
+        entry = Entry.new(description: "1:1 with Amanda entry")
+        validator = TimeEntry::DescriptionRules.new(entry)
+        expect(validator.valid?).to eql(true)
+
+        entry = Entry.new(description: "Email/Slack catch up entry")
+        validator = TimeEntry::DescriptionRules.new(entry)
+        expect(validator.valid?).to eql(true)
+      end
+
       it "will fail if too short" do
         entry = Entry.new(description: "[OSS-136]")
         validator = TimeEntry::DescriptionRules.new(entry)
